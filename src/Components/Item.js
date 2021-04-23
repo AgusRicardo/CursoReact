843d706ec7f506fedc5065e661db7f83f69c39e2
Agustin Ricardo <agustinricardo14@gmail.com>
2021-04-23 12:54:00 -0300

    Contador

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import './Item.css'
+import ItemCount from './ItemCount'
 
 
-const Item = ({img,title,precio,id}) => {
-    console.log(img,title,precio,id);
+const Item = ({img,title,precio,id, stock}) => {
+    console.log(img,title,precio,id, stock);
     return (
         <React.Fragment>
             <div className='card'>
                 <div>
-                    <img src={img} alt="" className='img-card'/>
-                    <p>{title}</p>
-                    <p>{precio}</p>
+                    <div>
+                        <img src={img} alt="" className='img-card'/>
+                    </div>
+                    <div>
+                        <p>{title}</p>
+                        <p>{precio}</p>
+                    </div>
+                </div>
+                <div>
+                    <ItemCount stock={stock}/>
                 </div>
             </div>
         </React.Fragment>
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -12,18 +12,21 @@ const ItemListContainer = () => {
                 title: 'Remera',
                 precio: '$2500',
                 img: 'https://www.guantexindustrial.com.ar/700-large_default/remera-algodon-jersey-azul-talle-xxxl.jpg',
+                stock: 7,
             },
             {
                 id: 'agaskgabjewethka',
                 title: 'Pantalón',
                 precio: '$2000',
                 img: 'https://www.clementdesign.com/es/wp-content/uploads/sites/4/products/06CCYCL/06CCYCLNOIR/p_vface.png',
+                stock: 10,
             },
             {
                 id: 'agaskgaasdbjhka',
                 title: 'Campera',
                 precio: '$4500',
                 img: 'https://d2r9epyceweg5n.cloudfront.net/stores/143/626/products/campera-undead1-21373735edc521da4815450885284633-1024-1024.png',
+                stock: 4,
             }
         ]
         const productosPromise = new Promise((resolve, reject) => {
